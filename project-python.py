a2d2fe0026ecde112eec9ff513134d42910ccfbd
DiederikH <55525739+DiederikHarmsen@users.noreply.github.com>
2020-04-27 13:57:04 +0200

    Update project-python.py

--- project-python.py
+++ project-python.py
@@ -29,7 +29,7 @@ def executeScriptsFromFile(filename):
     for command in sqlCommands:
         try:
             c.execute(command)
-        except OperationalError, msg:
+        except OperationalError as msg:
             print
             "Command skipped: ", msg
 
@@ -71,6 +71,7 @@ def get_ingredients():
     response.status = 200
     return json.dumps({"ingredients": s}, indent=4)
 
+
 @get('/cookies')
 def get_cookies():
     c = conn.cursor()
@@ -80,14 +81,15 @@ def get_cookies():
             FROM Product
         """
     )
-    s = [{"name" :Product_name}
+    s = [{"name": Product_name}
          for (Product_name) in c]
 
     response.status = 200
     return json.dumps({"cookies": s}, indent=4)
 
+
 @get('/recipes')
-#Simple get recipes.
+# Simple get recipes.
 def get_recipes():
     c = conn.cursor()
     c.execute(
@@ -98,22 +100,28 @@ def get_recipes():
         USING(Ingredient_name)
         """
     )
-    s = [{"cooke" :Product_name,"ingredient":Ingredient_name, "quantity": Quantity,"unit": Unit}
+    s = [{"cookie": Product_name, "ingredient": Ingredient_name, "quantity": Quantity, "unit": Unit}
          for (Product_name, Ingredient_name, Quantity, Unit) in c]
 
     response.status = 200
     return json.dumps({"recipes": s}, indent=4)
 
+
+# curl -X POST http://localhost:8888/pallets\?cookie\=Berliner
+# create a new pallet with cookie berliner
+# if no cookie return status
+# if not enough ingred return status
+# if all good return ok
+# newly created pallets have blocked status false and can have customer as null.
 @post('/pallets')
 def post_pallets():
     response.content_type = 'application/json'
 
     cookie = request.query.cookie
 
-    if not (cookie):
+    if not cookie:
         response.status = 400
         return json.dump({"error": "missing parameter"}, indent=4)
-
 
     try:
         c = conn.cursor()
@@ -127,95 +135,98 @@ def post_pallets():
         )
         check = c.fetchone()[0]
     except:
-        return json.dumps({"status":"no such cookie"}, indent = 4)
+        return json.dumps({"status": "no such cookie"}, indent=4)
 
-    #loopa igenom produktens ingredienser och kor checkingredients.
-    c.execute("""
-    SELECT Ingredient_name, Quantity
+    # loopa igenom produktens ingredienser och kor checkingredients.
+    c.execute(
+        """
+    SELECT Ingredient_name
     FROM Recipe
-    WHERE Product_name = ?
+    INNER JOIN Ingredient
+    USING(Ingredient_name)
+    WHERE Product_name = ? AND (QuantityStorage - Quantity*54 <0 OR QuantityStorage is NULL)
     """, [cookie])
-    ingreds =  c.fetchall()
-    #the wished ingredients and the quantities of them are now in an arraylist. Now we wish to run them through the checkIngredients
-    #function to check if there are enough ingredients to produce the pallet.
-    x = [(1, 2), (3, 4), (5, 6)]
+    # We get the differences and put them into a list.
+    Ingreds = c.fetchall()
+    if Ingreds:
+        return json.dumps({"status": "not enough ingredients"})
 
-    for a, b in x:
-        print(a, "plus", b, "equals", a + b)
-    for IngName,Quant in ingreds:
-        if (checkIngredients(IngName,Quant) < 0):
-            response.status = 400
-            return json.dumps({"status": "not enough ingredients"}, indent=4)
-
-
-    return json.dumps({"status": "ok"}, indent=4)
-
-
-def checkIngredients(Ingredient, Quantity):
-    #We compare quantity that is to be withdrawn with the current quantity of the storage. Multiply quantity by 54 to get amount for
-    #5400 cookies and not for 100 cookies.
-    Quantity = Quantity*54
-    c = conn.cursor()
-    c.execute("""
-    SELECT QuantityStorage
-    FROM Ingredient
-    WHERE Ingredient_name = ?
-    -
-    ?
-    """,[Ingredient, Quantity]
-              )
-    return c.fetchone()[0]
+    # Kor programmet, skapar en pallet och drar bort quantity fran storage.
+    c.execute(
+        """
+        INSERT INTO Pallet(Palletid, Product_name, ProductionDate)
+        VALUES(lower(hex(randomblob(16))), ?, CURRENT_DATE )
+        RETURNING Palletid;
+        
+        SELECT Palletid
+        FROM Pallet
+        WHERE rowid = last_insert_rowid()
+        """, [cookie]
+    )
+    get_Palletid = c.fetchone()[0]
+    print(get_Palletid)
 
 
 
+    return json.dumps({"status": "ok", "id": get_Palletid})
 
 
-# curl -X POST http://localhost:8888/pallets\?cookie\=Berliner
-# create a new pallet with cookie berliner
-# if no cookie return status
-# if not enough ingred return status
-# if all good return ok
-# newly created pallets have blocked status false and can have customer as null.
+# simple get pallets. See if blocked or not as
 
-
+# curl -X GET http://localhost:8888/pallets\?cookie\=Amneris\&blocked\=0\&after\=2020-03-03
+# extra parameters are "blocked" see only blocked pallets, "after" get pallets produced after date.
+# "before" restricts search to before date. "cookie" gets pallet of specific cookie
 @get('/pallets')
 def get_pallets():
     c = conn.cursor()
     c.execute(
         """
-        SELECT Palletid, Product_name, ProductionDate, Name, BlockedStatus
+        SELECT Palletid, Product_name, ProductionDate, BlockedStatus
         FROM Pallet
-        INNER JOIN Order
-        USING(OrderId)
-        INNER JOIN Customer
-        USING(CustomerId)
+
         """
     )
-    s = [{"id":Palletid, "cookie":Product_name, "productionDate":ProductionDate,"customer":Name, "blocked":BlockedStatus}
-         for (Palletid,Product_name,ProductionDate, Name, BlockedStatus) in c]
-
-
+    s = [{"id": Palletid, "cookie": Product_name, "productionDate": ProductionDate,
+          "blocked": BlockedStatus}
+         for (Palletid, Product_name, ProductionDate, BlockedStatus) in c]
 
     response.status = 200
     return json.dumps({"pallets": s}, indent=4)
-#simple get pallets. See if blocked or not as well.
-#curl -X GET http://localhost:8888/pallets\?cookie\=Amneris\&blocked\=0\&after\=2020-03-03
-# extra parameters are "blocked" see only blocked pallets, "after" get pallets produced after date.
-# "before" restricts search to before date. "cookie" gets pallet of specific cookie
 
-@post('/block')
+
 # curl -X POST http://localhost:8888/block/<cookie-name>/<from-date>/<to-date>
 # blocks specific cookie produced during form-date to to-date.
-#change blocked status to true if cookie name and is produced during specific date.
+# change blocked status to true if cookie name and is produced during specific date.
+@post('/block')
+def post_block():
+    response.content_type = 'application/json'
 
+    cookie = request.query.cookie
+    from_date = request.query.from_date
+    to_date = request.query.to_date
+
+    if not (cookie and from_date and to_date):
+        response.status = 400
+        return json.dump({"error": "missing parameter"}, indent=4)
+
+    try:
+        c = conn.cursor()
+        c.execute(
+            """
+            UPDATE Pallet
+            SET
+                BlockedStatus = true
+            WHERE Product_name = ? AND  ProductionDate > ? AND ProductionDate < ?
+            """,
+            [cookie, from_date, to_date]
+        )
+        check = c.fetchone()[0]
+    except:
+        return json.dumps({"status": "no such cookie"}, indent=4)
 
 
 @post('/unblock')
-#unblocks cookies from dates to dates. Similar as before but with unblock insted.
-
-
-
-
+# unblocks cookies from dates to dates. Similar as before but with unblock insted.
 
 @post('/reset')
 def reset():
